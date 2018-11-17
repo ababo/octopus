ed385910502c0c879eefd30d9fb4ff94df3c1380
Simon Prykhodko <semion.ababo@gmail.com>
2018-11-17 23:47:17 +0100

    Define GDT and minimum sufficient page tables in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_main]
 #![no_std]
+#![feature(const_slice_len)]
 #![feature(global_asm)]
 #![feature(lang_items)]
 #![feature(linkage)]
@@ -7,6 +8,9 @@
 #[cfg(target_arch = "x86_64")]
 global_asm!(include_str!("start.x86_64.s"));
 
+#[cfg_attr(target_arch = "x86_64", path = "memory.x86_64.rs")]
+#[cfg_attr(target_arch = "aarch64", path = "memory.aarch64.rs")]
+mod memory;
 mod multiboot;
 
 use core::panic::PanicInfo;
