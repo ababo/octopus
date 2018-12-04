e43f20298f1484a274feccc2a1fd653fcf354a09
Simon Prykhodko <semion.ababo@gmail.com>
2018-12-04 22:11:58 +0100

    Implement DtbStructIterator::find with a test

--- src/device_tree/dtb_reader.rs
+++ src/device_tree/dtb_reader.rs
@@ -6,6 +6,7 @@ use super::common::*;
 use super::dtb_format::*;
 use super::struct_item::*;
 
+#[derive(Clone, Debug)]
 pub struct DtbStructIterator<'a> {
     struct_block: &'a [u8],
     strings_block: &'a [u8],
@@ -109,6 +110,43 @@ impl<'a> DtbStructIterator<'a> {
             };
         }
     }
+
+    pub fn find(
+        &self,
+        path: &str,
+    ) -> Result<(StructItem<'a>, DtbStructIterator<'a>)> {
+        let path = if path.ends_with("/") {
+            &path[..path.len() - 1]
+        } else {
+            path
+        };
+
+        let mut item = StructItem::EndNode;
+        let mut iter = self.clone();
+
+        for part in path.split("/") {
+            let mut level = 0;
+            loop {
+                item = iter.next()?;
+                match item {
+                    StructItem::BeginNode { name } => {
+                        if level == 0 && name == part {
+                            break;
+                        }
+                        level += 1;
+                    }
+                    StructItem::Property { name, value: _ } => {
+                        if level == 0 && name == part {
+                            break;
+                        }
+                    }
+                    StructItem::EndNode => level -= 1,
+                }
+            }
+        }
+
+        Ok((item, iter))
+    }
 }
 
 impl<'a> Iterator for DtbStructIterator<'a> {
@@ -129,116 +167,117 @@ pub struct DtbReader<'a> {
     strings_block: &'a [u8],
 }
 
-fn get_header<'a>(blob: &'a [u8]) -> Result<DtbHeader> {
-    if blob.len() < 4 {
-        return Err(Error::BadMagic);
-    }
-
-    let be_header =
-        unsafe { &*(blob.as_ptr() as *const DtbHeader) as &DtbHeader };
-
-    if u32::from_be(be_header.magic) != DTB_MAGIC {
-        return Err(Error::BadMagic);
-    }
-
-    if blob.len() < size_of::<DtbHeader>() {
-        return Err(Error::UnexpectedEndOfBlob);
-    }
-
-    let header = DtbHeader {
-        magic: DTB_MAGIC,
-        total_size: u32::from_be(be_header.total_size),
-        struct_offset: u32::from_be(be_header.struct_offset),
-        strings_offset: u32::from_be(be_header.strings_offset),
-        reserved_mem_offset: u32::from_be(be_header.reserved_mem_offset),
-        version: u32::from_be(be_header.version),
-        last_comp_version: u32::from_be(be_header.last_comp_version),
-        bsp_cpu_id: u32::from_be(be_header.bsp_cpu_id),
-        strings_size: u32::from_be(be_header.strings_size),
-        struct_size: u32::from_be(be_header.struct_size),
-    };
-
-    if header.version < header.last_comp_version {
-        return Err(Error::BadVersion);
-    }
-
-    if header.last_comp_version != DTB_COMP_VERSION {
-        return Err(Error::UnsupportedCompVersion);
-    }
-
-    if header.total_size as usize != blob.len() {
-        return Err(Error::BadTotalSize);
-    }
-
-    Ok(header)
-}
-
-fn get_reserved_mem<'a>(
-    blob: &'a [u8],
-    header: &DtbHeader,
-) -> Result<&'a [ReservedMemEntry]> {
-    let entry_size = size_of::<ReservedMemEntry>();
-    if header.reserved_mem_offset + entry_size as u32 > header.struct_offset {
-        return Err(Error::OverlappingReservedMem);
-    }
-
-    if header.reserved_mem_offset % 8 != 0 {
-        return Err(Error::UnalignedReservedMem);
-    }
-
-    let reserved_max_size =
-        (header.struct_offset - header.reserved_mem_offset) as usize;
-    let reserved = unsafe {
-        let ptr = blob.as_ptr().offset(header.reserved_mem_offset as isize)
-            as *const ReservedMemEntry;
-        from_raw_parts(ptr, reserved_max_size / entry_size)
-    };
-
-    let index = reserved
-        .iter()
-        .position(|ref e| e.address == 0 && e.size == 0);
-    if index.is_none() {
-        return Err(Error::NoZeroReservedMemEntry);
-    }
-
-    Ok(&reserved[..index.unwrap()])
-}
-
-fn get_struct_block<'a>(
-    blob: &'a [u8],
-    header: &DtbHeader,
-) -> Result<&'a [u8]> {
-    if header.struct_offset % 4 != 0 || header.struct_size % 4 != 0 {
-        return Err(Error::UnalignedStruct);
-    }
-
-    if header.struct_offset + header.struct_size > header.strings_offset {
-        return Err(Error::OverlappingStruct);
-    }
-
-    let offset = header.struct_offset as usize;
-    Ok(&blob[offset..offset + header.struct_size as usize])
-}
-
-fn get_strings_block<'a>(
-    blob: &'a [u8],
-    header: &DtbHeader,
-) -> Result<&'a [u8]> {
-    if header.strings_offset + header.strings_size > header.total_size {
-        return Err(Error::OverlappingStrings);
-    }
-
-    let offset = header.strings_offset as usize;
-    Ok(&blob[offset..offset + header.strings_size as usize])
-}
-
 impl<'a> DtbReader<'a> {
+    fn get_header(blob: &'a [u8]) -> Result<DtbHeader> {
+        if blob.len() < 4 {
+            return Err(Error::BadMagic);
+        }
+
+        let be_header =
+            unsafe { &*(blob.as_ptr() as *const DtbHeader) as &DtbHeader };
+
+        if u32::from_be(be_header.magic) != DTB_MAGIC {
+            return Err(Error::BadMagic);
+        }
+
+        if blob.len() < size_of::<DtbHeader>() {
+            return Err(Error::UnexpectedEndOfBlob);
+        }
+
+        let header = DtbHeader {
+            magic: DTB_MAGIC,
+            total_size: u32::from_be(be_header.total_size),
+            struct_offset: u32::from_be(be_header.struct_offset),
+            strings_offset: u32::from_be(be_header.strings_offset),
+            reserved_mem_offset: u32::from_be(be_header.reserved_mem_offset),
+            version: u32::from_be(be_header.version),
+            last_comp_version: u32::from_be(be_header.last_comp_version),
+            bsp_cpu_id: u32::from_be(be_header.bsp_cpu_id),
+            strings_size: u32::from_be(be_header.strings_size),
+            struct_size: u32::from_be(be_header.struct_size),
+        };
+
+        if header.version < header.last_comp_version {
+            return Err(Error::BadVersion);
+        }
+
+        if header.last_comp_version != DTB_COMP_VERSION {
+            return Err(Error::UnsupportedCompVersion);
+        }
+
+        if header.total_size as usize != blob.len() {
+            return Err(Error::BadTotalSize);
+        }
+
+        Ok(header)
+    }
+
+    fn get_reserved_mem(
+        blob: &'a [u8],
+        header: &DtbHeader,
+    ) -> Result<&'a [ReservedMemEntry]> {
+        let entry_size = size_of::<ReservedMemEntry>();
+        if header.reserved_mem_offset + entry_size as u32 > header.struct_offset
+        {
+            return Err(Error::OverlappingReservedMem);
+        }
+
+        if header.reserved_mem_offset % 8 != 0 {
+            return Err(Error::UnalignedReservedMem);
+        }
+
+        let reserved_max_size =
+            (header.struct_offset - header.reserved_mem_offset) as usize;
+        let reserved = unsafe {
+            let ptr = blob.as_ptr().offset(header.reserved_mem_offset as isize)
+                as *const ReservedMemEntry;
+            from_raw_parts(ptr, reserved_max_size / entry_size)
+        };
+
+        let index = reserved
+            .iter()
+            .position(|ref e| e.address == 0 && e.size == 0);
+        if index.is_none() {
+            return Err(Error::NoZeroReservedMemEntry);
+        }
+
+        Ok(&reserved[..index.unwrap()])
+    }
+
+    fn get_struct_block(
+        blob: &'a [u8],
+        header: &DtbHeader,
+    ) -> Result<&'a [u8]> {
+        if header.struct_offset % 4 != 0 || header.struct_size % 4 != 0 {
+            return Err(Error::UnalignedStruct);
+        }
+
+        if header.struct_offset + header.struct_size > header.strings_offset {
+            return Err(Error::OverlappingStruct);
+        }
+
+        let offset = header.struct_offset as usize;
+        Ok(&blob[offset..offset + header.struct_size as usize])
+    }
+
+    fn get_strings_block(
+        blob: &'a [u8],
+        header: &DtbHeader,
+    ) -> Result<&'a [u8]> {
+        if header.strings_offset + header.strings_size > header.total_size {
+            return Err(Error::OverlappingStrings);
+        }
+
+        let offset = header.strings_offset as usize;
+        Ok(&blob[offset..offset + header.strings_size as usize])
+    }
+
     pub fn new(blob: &'a [u8]) -> Result<Self> {
-        let header = get_header(blob)?;
+        let header = DtbReader::get_header(blob)?;
         Ok(DtbReader::<'a> {
-            reserved_mem: get_reserved_mem(blob, &header)?,
-            struct_block: get_struct_block(blob, &header)?,
-            strings_block: get_strings_block(blob, &header)?,
+            reserved_mem: DtbReader::get_reserved_mem(blob, &header)?,
+            struct_block: DtbReader::get_struct_block(blob, &header)?,
+            strings_block: DtbReader::get_strings_block(blob, &header)?,
         })
     }
 
@@ -431,5 +470,68 @@ mod tests {
         assert_eq!(iter.next().unwrap(), StructItem::EndNode);
         assert_eq!(iter.next().unwrap_err(), Error::NoMoreStructItems);
         assert_eq!(iter.next().unwrap_err(), Error::NoMoreStructItems);
+    }
+
+    fn assert_not_found<'a>(iter: &DtbStructIterator<'a>, path: &str) {
+        let err = iter.find(path).unwrap_err();
+        assert_eq!(err, Error::NoMoreStructItems);
+    }
+
+    fn name_from_path<'a>(path: &'a str) -> &'a str {
+        path.trim_end_matches("/").rsplit("/").next().unwrap()
+    }
+
+    fn assert_begin_node_found<'a>(
+        iter: &DtbStructIterator<'a>,
+        path: &str,
+    ) -> DtbStructIterator<'a> {
+        let (item, iter) = iter.find(path).unwrap();
+        assert!(item.is_begin_node());
+        assert_eq!(item.name().unwrap(), name_from_path(path));
+        iter
+    }
+
+    fn assert_property_found<'a>(
+        iter: &DtbStructIterator<'a>,
+        path: &str,
+        value: &[u8],
+    ) {
+        let (item, _) = iter.find(path).unwrap();
+        assert!(item.is_property());
+        assert_eq!(item.name().unwrap(), name_from_path(path));
+        assert_eq!(item.value().unwrap(), value);
+    }
+
+    #[test]
+    fn test_find() {
+        let mut buf = Vec::new();
+        let root = new_reader(&mut buf, "sample").unwrap().struct_iter();
+
+        assert_begin_node_found(&root, "");
+        assert_begin_node_found(&root, "/");
+        assert_not_found(&root, "//");
+
+        let iter = assert_begin_node_found(&root, "/node1");
+        assert_not_found(&root, "node1");
+
+        let val = "A string\0".as_bytes();
+        assert_not_found(&root, "/a-string-property");
+        assert_not_found(&root, "a-string-property");
+        assert_property_found(&iter, "a-string-property", val);
+        assert_property_found(&root, "/node1/a-string-property", val);
+
+        let iter = assert_begin_node_found(&root, "/node2");
+        assert_begin_node_found(&root, "/node2/");
+        assert_property_found(&root, "/node2/an-empty-property", &[]);
+        assert_property_found(&iter, "an-empty-property", &[]);
+        assert_not_found(&root, "an-empty-property");
+        assert_not_found(&iter, "/node2/an-empty-property");
+
+        assert_not_found(&iter, "node1/child-node1/a-string-property");
+        assert_property_found(
+            &root,
+            "/node1/child-node1/a-string-property/",
+            "Hello, world\0".as_bytes(),
+        );
     }
 }
